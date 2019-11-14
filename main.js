1fde37582a974704c94e81b59a5d79c986a6a27c
keyhoh <iheyhoh+github@gmail.com>
2019-11-14 21:38:09 +0900

    bundle

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const antiPhishing = require("./lib");
 const resultList = antiPhishing.check(window.location.href);
-if (resultList.filter(result.similarity === "SAME").length === 0) {
+if (resultList.filter(result => result.similarity === "SAME").length === 0) {
     resultList.forEach(result => {
         if (result.similarity === "SIMILAR") {
             alert(`このサイトは${result.title}の正規のサイトではない可能性があります。`);
