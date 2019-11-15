e9541dc6076a8cd88d75f41a00b8f1fa8583f496
keyhoh <iheyhoh+github@gmail.com>
2019-11-15 16:10:50 +0900

    restrict permissions

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const antiPhishing = require("./lib");
-const resultList = antiPhishing.check(window.location.href);
-if (resultList.filter(result => result.similarity === "SAME").length === 0) {
-    resultList.forEach(result => {
-        if (result.similarity === "SIMILAR") {
-            alert(`このサイトは${result.title}の正規のサイトではない可能性があります。`);
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+        const resultList = antiPhishing.check(tab.url);
+        if (resultList.filter(result => result.similarity === "SAME").length === 0) {
+            resultList.forEach(result => {
+                if (result.similarity === "SIMILAR") {
+                    alert(`このサイトは${result.title}の正規のサイトではない可能性があります。`);
+                }
+            });
         }
-    });
-}
+    }
+})
